a80916d9b866d90800bdf9e0268de8eafa1d9a35
Fenykepy <phiroom@lavilotte-rolle.fr>
2016-08-21 21:55:21 +0200

    add overlay to content.

--- content.js
+++ content.js
@@ -1,13 +1,37 @@
-// when we receive a message from browser action
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if (request.message === "clicked_browser_action") {
-      // we list all images in current page
-      var imgs = document.getElementsByTagName('img');
-      for (var i=0, l=imgs.length; i < l; i++) {
-        console.log(imgs[i].src);
-      }     
+// we encapsulate everything to avoid polluting global scope
+(function() {
+  // when we receive a message from browser action
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if (request.message === "clicked_browser_action") {
+        console.log('add ui');
+        addUI();
+      }
     }
+  );
+
+  function scanForImages() {
+    // we get all images
+    var imgs = document.getElementsByTagName('img');
+    // we get all links
+    var links = document.getElementsByTagName('a');
+
   }
-);
+
+  function addUI() {
+    // we create div overlay
+    var overlay = document.createElement("div");
+    overlay.setAttribute("id", "pinpict_overlay");
+    
+    // we add div element to body
+    document.body.appendChild(overlay);
+
+
+  }
+
+  function removeUI() {
+    document.getElementByID("pinpict_overlay").remove();
+  }
+})(this);
+
 
